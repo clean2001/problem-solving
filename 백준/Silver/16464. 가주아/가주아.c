01c09e1d88de215396316d585aa97c9c090f95b5
Sejeong <64718002+clean2001@users.noreply.github.com>
2022-03-22 11:33:03 +0900

    [Silver III] Title: 가주아, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/16464. 가주아/가주아.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//가주아 블랙잭
+int main(void)
+{
+	int testCase;
+	int i = 0;
+	int num;
+
+	scanf("%d", &testCase);
+
+	i = 0;
+	while (i < testCase)
+	{
+		scanf("%d", &num);
+
+		while (num % 2 == 0)
+			num /= 2;
+
+		if (num != 1)
+			printf("Gazua\n");
+		else
+			printf("GoHanGang\n");
+
+		i++;
+
+	}
+	return 0;
+}
