90da0332fd84d03f56fbd6c133efcd3e813fc7fc
Sejeong <64718002+clean2001@users.noreply.github.com>
2022-07-18 20:47:13 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.cc
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -1,29 +1,66 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
 using namespace std;
+
+const int MAX = 100000;
+
+int h[MAX+1];
+int size_h;
+
+int deleteMax() { /*percolating down*/
+    if(size_h == 0) return 0;
+
+    int last, max;
+    max = h[1];
+    last = h[size_h--];
+
+    // for(int i=1; i<=size_h; ++i) cout << h[i] <<' ';
+    // cout << '\n';
+
+    int i, child;
+    for(i = 1; i*2 <= size_h; i = child) {
+        child = i*2;
+        if(child < size_h && h[child] < h[child+1]) child++;
+
+        if(h[child] > last) h[i] = h[child];
+        else break;
+    }
+
+    h[i] = last;
+
+    // cout << "after:\n";
+    // for(int i=1; i<=size_h; ++i) cout << h[i] <<' ';
+    // cout << '\n';
+
+    return max;
+}
+
+void insert(int n) { /*percolating up*/
+    int i;
+    for(i = ++size_h; i/2 >= 1 && h[i/2] < n; i/=2) {
+        h[i] = h[i/2];
+    }
+
+    h[i] = n;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    priority_queue<int> pq;
-
+    
     int N;
     cin >> N;
+
+    h[0] = 0;
+    size_h = 0;
 
     for(int i=0; i<N; ++i) {
         int x;
         cin >> x;
-        if(x==0 && pq.empty()) cout << 0 << '\n';
-        else if(x==0 && !pq.empty()) {
-            cout << pq.top() << '\n';
-            pq.pop();
-        }
-        else {
-            pq.push(x);
-        }
 
+        if(x == 0) cout << deleteMax() << '\n';
+        else insert(x);
     }
 
     return 0;
