dfa264c248534ee47b3036bffe852f1ce6db7e07
Sejeong <64718002+clean2001@users.noreply.github.com>
2022-08-25 01:55:26 +0900

    [Gold III] Title: 웜홀, Time: 28 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N, M, W;
+
+vector<pair<int, int>> edges[501];
+
+const int INF = 1e9;
+int dist[501];
+
+bool bf(int start) {
+	dist[start] = 0;
+
+	for(int k=1; k<=(N-1); ++k) {
+		for(int i=1; i<=N; ++i) {
+			for(pair<int, int> ed : edges[i]) {
+				if(dist[ed.first] > dist[i] + ed.second) {
+					dist[ed.first] = dist[i] + ed.second;
+				}
+			}
+		}
+	}
+
+	for(int i = 1; i <= N; ++i) {
+        for(pair<int, int> ed : edges[i]) {
+            if(dist[ed.first] > dist[i] + ed.second) {
+                return true;
+            }
+        }
+    }
+
+	return false;
+
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;
+	cin >> T;
+	for(int tc = 0; tc < T; ++tc) {
+
+		fill(dist, dist+501, INF);
+		for(int i=0; i<501; ++i) edges[i].clear();
+
+		cin >> N >> M >> W;
+
+		for(int i=0; i<M; ++i) {
+			int s, e, t;
+			cin >> s >> e >> t;
+			
+			edges[s].push_back({e, t});
+			edges[e].push_back({s, t});
+		}
+
+		for(int i=0; i<W; ++i) {
+			int s, e, t;
+			cin >> s >> e >> t;
+			edges[s].push_back({e, -t});
+		}
+
+		bool is_negative = bf(1);
+
+		if(is_negative) {
+			cout << "YES\n";
+		} else {
+			cout << "NO\n";
+		}
+	}
+
+	
+	return 0;
+}
