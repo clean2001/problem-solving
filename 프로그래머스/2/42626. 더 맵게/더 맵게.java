4a643064137fcd03287f74d7471789fd4fb83c50
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-06-11 17:26:04 +0900

    [level 2] Title: 더 맵게, Time: 1662.08 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,28 +1,21 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
+        Queue<Integer> pq = new PriorityQueue<>();
         for(int s : scoville) {
             pq.add(s);
         }
         
         int cnt = 0;
-        
-        while(pq.size() > 1) {
-            int first = pq.poll(), second = pq.poll();
-            // System.out.println(first + " " + second);
-            if(first >= K) {
-                pq.add(first); pq.add(second);
-                break;
-            }
-            
-            int newSc = first + second * 2;
-            pq.add(newSc);
+        while(pq.size() > 1 && pq.peek() < K) {
+            int first = pq.poll();
+            int second = pq.poll();
+            pq.add(first + second * 2);
             cnt++;
         }
         
-        if(pq.poll() < K) return -1;
+        if(pq.peek() < K) return -1;
         
         return cnt;
     }
