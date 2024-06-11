52e3bf3ed95c663b85f55b5819398c4330e85e64
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-06-11 14:50:09 +0900

    [level 2] Title: 전화번호 목록, Time: 218.52 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,26 +1,18 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        int len = phone_book.length;
-        // 해시 맵 만들기
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<len; ++i) {
-            String sub = "";
-            for(int j=phone_book[i].length()-1; j>=0; --j) {
-                sub = phone_book[i].substring(0, j);
-                if(map.get(sub) != null) break;
-                map.put(sub, i);
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length-1; ++i) {
+            String cur = phone_book[i];
+            String next = phone_book[i+1];
+            
+            if(cur.length() <= next.length()) {
+                String subNext = next.substring(0, cur.length());
+                if(subNext.equals(cur))
+                    return false;
             }
         }
         
-        boolean ans = true;
-        for(int i=0; i<len; ++i) {
-            Integer idx = map.get(phone_book[i]);
-            if(idx != null && idx != i) {
-                ans = false;
-                break;
-            }
-        }
-        return ans;
+        return true;
     }
 }
