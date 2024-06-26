f569bd64f7d3eff3f7e06dddf0a69747b6abe820
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-06-26 17:47:15 +0900

    [level 3] Title: 등굣길, Time: 0.98 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 52.6 MB, 시간: 1.13 ms
+메모리: 52.6 MB, 시간: 0.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 26일 17:22:48
+2024년 06월 26일 17:47:13
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -11,29 +11,26 @@ class Solution {
             dp[y][x] = -1;
         }
     
-        
         // dp 배열 채우기
         for(int i=0; i<n; ++i) {
             for(int j=0; j<m; ++j) {
-                if(dp[i][j] == -1) continue;
-                if(i == 0 && j == 0) {
+                if(i==0 && j==0) {
                     dp[i][j] = 1;
                     continue;
-                } else if(i == 0) {
-                    dp[i][j] = Math.max(0, dp[i][j-1]);
-                    continue;
-                } else if(j == 0) {
-                    dp[i][j] = Math.max(0, dp[i-1][j]);
+                }
+                if(dp[i][j] == -1) {
+                    dp[i][j] = 0;
                     continue;
                 }
                 
-                int m1 = Math.max(dp[i-1][j], 0) % 1000000007;
-                int m2 = Math.max(dp[i][j-1], 0) % 1000000007;
+                int m1 = i >= 1 ? Math.max(dp[i-1][j], 0) % 1000000007 : 0;
+                int m2 = j >= 1 ? Math.max(dp[i][j-1], 0) % 1000000007 : 0;
                 
                 dp[i][j] = (m1 + m2) % 1000000007;
             }
         }
         
+        // System.out.println(Arrays.deepToString(dp));
         return dp[n-1][m-1];
     }
 }
