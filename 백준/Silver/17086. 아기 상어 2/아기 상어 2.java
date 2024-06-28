cafaf7704e77c5c3be2d715a14cd47243a4dc38e
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-06-28 09:46:41 +0900

    [Silver II] Title: 아기 상어 2, Time: 140 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -8,6 +8,9 @@ class Main {
 
   static int[] dy = {-1, -1, -1, 0, 1, 1, 1, 0};
   static int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
+  static Queue<int[]> q = new LinkedList<>();
+  static boolean[][] vis;
+  static int oneCnt = 0;
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,61 +20,43 @@ class Main {
     M = Integer.parseInt(st.nextToken());
 
     map = new int[N][M];
+    vis = new boolean[N][M];
 
     for(int i=0; i<N; ++i) {
       st = new StringTokenizer(br.readLine());
       for(int j=0; j<M; ++j) {
         map[i][j] = Integer.parseInt(st.nextToken());
+        if(map[i][j] == 1) {
+          q.add(new int[]{i, j, 0});
+          vis[i][j] = true;
+          oneCnt++;
+        }
+
       }
     }
 
-    ans = Integer.MIN_VALUE;
-    for(int i=0; i<N; ++i) {
-      for(int j=0; j<M; ++j) {
-        if(map[i][j] == 1) continue;
-        ans = Math.max(ans, bfs(i, j));
-      }
-    }
-
-    System.out.println(ans);
+    System.out.println(bfs());
   }
 
-  static int bfs(int y, int x) {
-    int[][] dist = new int[N][M];
-
-    for(int i=0; i<N; ++i) {
-      for(int j=0; j<M; ++j) {
-        dist[i][j] = -1;
-      }
-    }
-    dist[y][x] = 0;
-    Queue<int[]> q = new LinkedList<>();
-    q.add(new int[] {y, x});
+  static int bfs() {
+    int ans = Integer.MIN_VALUE;
     while(!q.isEmpty()) {
       int[] cur = q.poll();
-      int cy = cur[0], cx = cur[1];
+      int cy = cur[0], cx = cur[1], d = cur[2];
 
       for (int i = 0; i < 8; ++i) {
         int ny = cy + dy[i];
         int nx = cx + dx[i];
 
-        if (ny >= N || ny < 0 || nx >= M || nx < 0)
-          continue;
-        if (dist[ny][nx] != -1)
-          continue;
+        if (ny >= N || ny < 0 || nx >= M || nx < 0) continue;
+        if (vis[ny][nx]) continue;
 
-        if (map[ny][nx] == 1) { // 상어 만남
-          return dist[cy][cx] + 1;
-        }
-
-        q.add(new int[]{ny, nx});
-        dist[ny][nx] = dist[cy][cx] + 1;
+        q.add(new int[]{ny, nx, d+1});
+        ans = Math.max(ans, d+1);
+        vis[ny][nx] = true;
       }
     }
-
-
-
-    return -1;
+    return ans;
   }
 
   static void print(int[][] dist) {
