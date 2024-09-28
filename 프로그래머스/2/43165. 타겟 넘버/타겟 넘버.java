cb28679ac43442466432ce8b0324bf7e813cf594
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-09-28 20:47:54 +0900

    [level 2] Title: 타겟 넘버, Time: 4.62 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,18 @@
 class Solution {
-    private static int ans = 0;
+    static int ans = 0;
     public int solution(int[] numbers, int target) {
         dfs(0, 0, numbers, target);
-        
         return ans;
     }
     
-    private static void dfs(int depth, int curValue, int[] numbers, int target) {
-        if(depth == numbers.length) {
-            if(curValue == target) {
-                ans++;
-            }
-            
+    private void dfs(int index, int cur, int[] numbers, int target) {
+        if(index == numbers.length) {
+            if(target == cur) ans++;
             return;
         }
         
-        dfs(depth+1, curValue + numbers[depth], numbers, target);
-        dfs(depth+1, curValue - numbers[depth], numbers, target);
+        dfs(index+1, cur+numbers[index], numbers, target);
+        dfs(index+1, cur-numbers[index], numbers, target);
+        
     }
 }
