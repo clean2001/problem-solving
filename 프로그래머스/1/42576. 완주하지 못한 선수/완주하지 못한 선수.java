d8d4ba42edde2cae12b9c6980975de5e0a266ad2
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-09-28 13:32:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.66 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,22 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> completedMap = new HashMap<>();
-        
-        // 완주한 선수의 이름과 그 수를 저장(선수 이름 중복 고려)
-        for(String c : completion) {
-            completedMap.put(c, completedMap.getOrDefault(c, 0) + 1);
+        Map<String, Integer> c = new HashMap<>();
+        for(String name : completion) {
+            int count = c.getOrDefault(name, 0);
+            
+            c.put(name, count+1);
         }
         
-        String ans = "";
-        for(String p : participant) {
-            Integer cnt = completedMap.get(p);
-            if(cnt == null || cnt == 0) {
-                ans = p; break;
-            } else {
-                completedMap.put(p, cnt-1);
+        String answer = "";
+        
+        for(String name : participant) {
+            int count = c.getOrDefault(name, 0);
+            
+            if(count == 0) {
+                answer = name;
+                break;
             }
-        }
-        return ans;
+            
+            c.put(name, count - 1);
+        } 
+        
+        return answer;
     }
 }
