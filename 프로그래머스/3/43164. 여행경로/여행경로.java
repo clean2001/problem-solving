7fa09a4676b7eb332d3b2532275d50ba5d679c96
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-09-29 20:24:15 +0900

    [level 3] Title: 여행경로, Time: 74.52 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,33 +1,53 @@
 import java.util.*;
 
 class Solution {
-    static String[][] ticket;
-    static boolean[] vis;
-    static List<String> ans; 
+    public static Deque<String> ans;
+    public static List<String> ans1;
     public String[] solution(String[][] tickets) {
-        ticket = tickets;
-        vis = new boolean[tickets.length];
-        ans = new ArrayList<>();
+        ans = new LinkedList<>();
+        ans1 = new ArrayList<>();
+        // 뒤를 기준으로 오름차순 정렬
+        Arrays.sort(tickets, (a, b) -> {
+            return a[1].compareTo(b[1]);
+        });
         
-        dfs("ICN", 0, "ICN");
-        Collections.sort(ans);
-        
-        return ans.get(0).split(" ");    
-    }
-    
-    private static void dfs(String cur, int depth, String list) {
-        
-        if(depth == ticket.length) {
-            ans.add(list);
-            return;
-        }
-        for(int i=0; i<ticket.length; ++i) {
-            if(!vis[i] && ticket[i][0].equals(cur)) {
-                vis[i] = true;
-                dfs(ticket[i][1], depth+1, list + " " + ticket[i][1]);
-                vis[i] = false;
+        for(int i=0; i<tickets.length; ++i) {
+            if(tickets[i][0].equals("ICN")) {
+                dfs(i, new boolean[tickets.length], tickets, 0);
             }
         }
+        
+        String[] answer = new String[ans1.size()];
+        for(int i=0; i<ans1.size(); ++i) {
+            answer[i] = ans1.get(i);
+        }
+        
+        return answer;
+    }
+    
+    public static void dfs(int cur, boolean[] vis, String[][] tickets, int cnt) {
+        vis[cur] = true;
+        ans.addLast(tickets[cur][0]);
+        if(cnt == tickets.length-1) {
+            ans.add(tickets[cur][1]);
+            
+            if(ans1.isEmpty()) {
+                for(String a : ans) {
+                    ans1.add(a);
+                }
+                return;
+            }
 
+        }
+        
+        for(int i=0; i<tickets.length; ++i) {
+            if(vis[i]) continue;
+            if(tickets[i][0].equals(tickets[cur][1])) { // 일치
+                dfs(i, vis, tickets, cnt+1);
+            }
+        }
+        
+        vis[cur] = false;
+        ans.pollLast();
     }
 }
