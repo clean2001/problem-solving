f0ff145f45f355462ab844e2d70281e5bb469189
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-10-01 20:37:18 +0900

    [level 3] Title: 베스트앨범, Time: 12.98 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 1.60 ms
+메모리: 78.1 MB, 시간: 12.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 28일 14:44:00
+2024년 10월 01일 20:37:16
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,63 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public static class Genre {
-        public String gen;
-        public int cnt;
+    public static class Music {
+        String genre;
+        int total;
+        List<int[]> arr;
         
-        public Genre(String gen, int cnt) {
-            this.gen = gen;
-            this.cnt = cnt;
+        public Music(String genre, int total) {
+            this.genre = genre;
+            this.total = total;
+            arr = new ArrayList<>();
         }
-        
     }
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> cntMap = new HashMap<>(); // 장르, 총수
-        Map<String, PriorityQueue<int[]>> genMap = new HashMap<>(); // 장르, 장르별 노래
-        
+        Map<String, Music> map = new HashMap<>();
         for(int i=0; i<genres.length; ++i) {
-            int cnt = cntMap.getOrDefault(genres[i], 0);
-            cntMap.put(genres[i], cnt+plays[i]);
-            
-            PriorityQueue<int[]> pq = genMap
-                .getOrDefault(genres[i], new PriorityQueue<int[]>((a, b) -> {
-                if(a[1] == b[1]) {
-                    return a[0] - b[0];
-                }
-                return b[1] - a[1];
-            }));
-            pq.add(new int[] {i, plays[i]});
-            genMap.put(genres[i], pq);
+            Music m = map.getOrDefault(genres[i], null);
+            if(m == null) {
+                m = new Music(genres[i], plays[i]);
+                m.arr.add(new int[]{i, plays[i]});
+                map.put(genres[i], m);
+            } else {
+                m.arr.add(new int[]{i, plays[i]});
+                m.total += plays[i];
+            }
         }
         
-        // 가장 많은 장르 찾기
-        PriorityQueue<Genre> genPq = new PriorityQueue<Genre>((a, b) -> {
-            return b.cnt - a.cnt;
+        PriorityQueue<Music> pq = new PriorityQueue<>((a, b) -> {
+            return b.total - a.total;
         });
         
-        for(Map.Entry<String, Integer> entry : cntMap.entrySet()) {
-            genPq.add(new Genre(entry.getKey(), entry.getValue()));
+        for(Map.Entry<String, Music> e : map.entrySet()) {
+            pq.add(e.getValue());
         }
-        // System.out.println(genMap);
-        ArrayList<Integer> ans = new ArrayList<>();
-        while(!genPq.isEmpty()) {
-            Genre cur = genPq.poll();
-            PriorityQueue<int[]> tempPq = genMap.get(cur.gen);
+        
+        ArrayList<Integer> ansList = new ArrayList<>();
+        while(!pq.isEmpty()) {
+            Music cur = pq.poll();
             
+            // 많은 순으로 정렬
+            Collections.sort(cur.arr, (a, b) -> {
+               return b[1] - a[1]; 
+            });
             
-            
-            if(tempPq.size() == 1) {
-                ans.add(tempPq.poll()[0]); continue;
-            }
-            
-            for(int i=0; i<2; ++i) {
-                ans.add(tempPq.poll()[0]);
+            for(int i=0; i<cur.arr.size(); ++i) {
+                ansList.add(cur.arr.get(i)[0]);
+                if(i == 1) break;
             }
         }
         
-        int[] answer = new int[ans.size()];
-        for(int i=0; i<ans.size(); ++i) {
-            answer[i] = ans.get(i);
+        int[] answer = new int[ansList.size()];
+        for(int i=0; i<ansList.size(); ++i) {
+            answer[i] = ansList.get(i);
         }
         
         return answer;
