7a108a424dd3499f0bc165d3e75b5fffb7ba1051
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-10-01 21:23:45 +0900

    [level 3] Title: 등굣길, Time: 2.04 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,36 +1,27 @@
-import java.util.*;
-
 class Solution {
+    static int D = 1_000_000_007;
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n][m];
-        
-        for(int[] p : puddles) {
-            int y = p[1] - 1;
-            int x = p[0] - 1;
-            
-            dp[y][x] = -1;
+        long[][] map = new long[n][m];
+        boolean[][] p = new boolean[n][m];
+        // 웅덩이 표시하기
+        for(int[] pd : puddles) {
+            p[pd[1]-1][pd[0]-1] = true;
         }
-    
-        // dp 배열 채우기
         for(int i=0; i<n; ++i) {
             for(int j=0; j<m; ++j) {
-                if(i==0 && j==0) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-                if(dp[i][j] == -1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
+                if(p[i][j]) continue;
                 
-                int m1 = i >= 1 ? Math.max(dp[i-1][j], 0) % 1000000007 : 0;
-                int m2 = j >= 1 ? Math.max(dp[i][j-1], 0) % 1000000007 : 0;
-                
-                dp[i][j] = (m1 + m2) % 1000000007;
+                if(i==0 && j == 0) {
+                    map[i][j] = 1L;
+                } else if(i == 0) {
+                    map[i][j] = map[i][j-1];
+                } else if(j == 0) {
+                    map[i][j] = map[i-1][j];
+                } else {
+                    map[i][j] = (map[i-1][j] % D + map[i][j-1] % D) % D;
+                }
             }
         }
-        
-        // System.out.println(Arrays.deepToString(dp));
-        return dp[n-1][m-1];
+        return (int)(map[n-1][m-1] % D);
     }
 }
