5521c82e77b91dd03193bd0af64720ded3e32495
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-10-01 22:15:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.11 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,40 @@
 import java.util.*;
 
 class Solution {
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, 1, -1};
+    public static int[] dy = {1, -1, 0, 0};
+    public static int[] dx = {0, 0, 1, -1};
+    
     public int solution(int[][] maps) {
-        return bfs(maps);
+        return bfs(0, 0, maps);
     }
     
-    private static int bfs(int[][] maps) {
+    public static int bfs(int y, int x, int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {0, 0, 1}); // y, x, dist;
-        boolean[][] vis = new boolean[maps.length][maps[0].length];
-        vis[0][0] = true;
-        
+        q.add(new int[]{y, x});
+        int[][] dist = new int[maps.length][maps[0].length];
+        dist[y][x] = 1;
+        int maxY = maps.length;
+        int maxX = maps[0].length;
         while(!q.isEmpty()) {
             int[] cur = q.poll();
-            
             for(int i=0; i<4; ++i) {
                 int ny = cur[0] + dy[i];
                 int nx = cur[1] + dx[i];
                 
-                if(ny >= maps.length || nx >= maps[0].length
-                   || ny < 0 || nx < 0) continue;
-                if(maps[ny][nx] == 0) continue;
-                if(vis[ny][nx]) continue;
-                
-                if(ny == maps.length-1 && nx == maps[0].length-1) {
-                    return cur[2] + 1;
+                if(ny >= maxY || ny < 0 || nx >= maxX || nx < 0) {
+                    continue;
                 }
+                if(dist[ny][nx] > 0) continue;
+                if(maps[ny][nx] == 0) continue;
                 
-                vis[ny][nx] = true;
-                q.add(new int[] {ny, nx, cur[2] + 1});
+                dist[ny][nx] = dist[cur[0]][cur[1]] + 1;
+                
+                if(ny == maxY-1 && nx == maxX-1) return dist[ny][nx];
+                
+                q.add(new int[]{ny, nx});
             }
         }
+        
         return -1;
     }
-    
 }
