fb850825ad299992d5ceb62ecc2071c11719ac85
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-10-01 22:31:59 +0900

    [level 3] Title: 가장 먼 노드, Time: 202.54 ms, Memory: 99.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,45 +1,49 @@
 import java.util.*;
+
 class Solution {
-    static List<List<Integer>> adj;
+    public static List<List<Integer>> adj;
     public int solution(int n, int[][] edge) {
         adj = new ArrayList<>();
+        for(int i=0; i<=n; ++i) {
+            adj.add(new ArrayList<>());
+        }
         
-        for(int i=0; i<=n; ++i) adj.add(new ArrayList<>());
         for(int[] e : edge) {
             adj.get(e[0]).add(e[1]);
             adj.get(e[1]).add(e[0]);
         }
         
-        return bfs(1, n);
+        return bfs(n);
+        
     }
     
-    private static int bfs(int start, int end) {
-        Queue<Integer> q = new LinkedList<>();
-        int maxDist = -1;
-        int ret = 0;
-        int[] dist = new int[end+1];
-        for(int i=0; i<end+1; ++i) dist[i] = -1;
-    
-        q.add(start);
-        dist[start] = 0;
+    public static int bfs(int n) {
+        int ans = 0;
+        int maxDist = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{1, 0}); // 노드, dist
+        
+        boolean[] vis = new boolean[n+1];
+        vis[1] = true;
         while(!q.isEmpty()) {
-            int cur = q.poll();
-            if(dist[cur] == maxDist) {
-                ret++;
-            } else if(dist[cur] > maxDist) {
-                maxDist = dist[cur];
-                ret = 1;
-            }
-            
-            List<Integer> nexts = adj.get(cur);
+            int[] cur = q.poll();
+            List<Integer> nexts = adj.get(cur[0]);
             for(int next : nexts) {
-                if(dist[next] >= 0) continue;
+                if(vis[next]) continue;
                 
-                q.add(next);
-                dist[next] = dist[cur] + 1;
+                int dist = cur[1] + 1;
+                vis[next] = true;
+                q.add(new int[]{next, dist});
+                
+                if(dist == maxDist) {
+                    ans++;
+                } else if(dist > maxDist) {
+                    maxDist = dist;
+                    ans = 1;
+                }
             }
         }
         
-        return ret;
+        return ans;
     }
 }
