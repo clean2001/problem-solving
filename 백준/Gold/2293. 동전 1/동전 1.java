7862bb3d552c4335d4b9bc91fc64f95e6c776816
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-10-05 08:28:47 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,30 +1,34 @@
-import java.io.*;
-import java.nio.Buffer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
+
 class Main {
+
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     StringTokenizer st = new StringTokenizer(br.readLine());
     int n = Integer.parseInt(st.nextToken());
     int k = Integer.parseInt(st.nextToken());
+    
+
     int[] dp = new int[k+1];
-    int[] coins = new int[n];
-//    System.out.println(n + " " + k);
+    int[] coin = new int[n];
 
     for(int i=0; i<n; ++i) {
-      coins[i] = Integer.parseInt(br.readLine());
+      coin[i] = Integer.parseInt(br.readLine());
     }
 
     dp[0] = 1;
     for(int i=0; i<n; ++i) {
-      for(int j=coins[i]; j <= k; j++) {
-        dp[j] += dp[j-coins[i]];
+      for(int j=coin[i]; j<=k; ++j) {
+        dp[j] += dp[j-coin[i]];
       }
     }
 
     System.out.println(dp[k]);
-
-
   }
+
 }
