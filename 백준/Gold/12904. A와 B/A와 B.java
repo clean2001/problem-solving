c4036f4727ee735b3492460597db06510e2d8212
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-11-08 21:21:12 +0900

    [Gold V] Title: A와 B, Time: 116 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -1,29 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Main {
-  static int N, k;
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    StringBuilder sb1 = new StringBuilder(br.readLine());
-    StringBuilder sb2 = new StringBuilder(br.readLine());
+    String S, T;
+    S = br.readLine();
+    T = br.readLine();
 
-    // sb2 -> sb1으로 가기!
-    while(sb1.length() != sb2.length()) {
-//      System.out.println(sb1.length() + " " + sb2.length());
-      if(sb2.charAt(sb2.length()-1) == 'A') { // 맨뒤가 A이면, A떼기
-        sb2.delete(sb2.length()-1, sb2.length());
-      } else { // 맨뒤가 B이 B떼고 뒤집기
-        sb2.delete(sb2.length()-1, sb2.length());
-        sb2.reverse();
+    StringBuilder sb = new StringBuilder(T);
+    while(sb.length() != S.length()) {
+      char last = sb.charAt(sb.length() - 1);
+      if(last == 'A') { // 그냥 떼기
+        sb.delete(sb.length() - 1, sb.length());
+      } else {
+        sb.delete(sb.length()-1, sb.length());
+        sb.reverse();
       }
     }
 
-    if(sb1.toString().equals(sb2.toString())) {
-      System.out.println("1");
+    if(sb.toString().equals(S)) {
+      System.out.println(1);
     } else {
-      System.out.println("0");
+      System.out.println(0);
     }
   }
 }
