37f839d41b23fb143ebc9117d9a238ed1f741163
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-11-08 12:54:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.45 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public static int[] dy = {1, -1, 0, 0};
-    public static int[] dx = {0, 0, 1, -1};
+    private static int[] dy = {1, -1, 0, 0};
+    private static int[] dx = {0, 0, 1, -1};
     
     public int solution(int[][] maps) {
-        return bfs(0, 0, maps);
-    }
-    
-    public static int bfs(int y, int x, int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{y, x});
-        int[][] dist = new int[maps.length][maps[0].length];
-        dist[y][x] = 1;
-        int maxY = maps.length;
-        int maxX = maps[0].length;
+        int[][] vis = new int[maps.length][maps[0].length];
+        
+        vis[0][0] = 1;
+        q.add(new int[]{0, 0});
         while(!q.isEmpty()) {
             int[] cur = q.poll();
             for(int i=0; i<4; ++i) {
                 int ny = cur[0] + dy[i];
                 int nx = cur[1] + dx[i];
                 
-                if(ny >= maxY || ny < 0 || nx >= maxX || nx < 0) {
+                if(ny >= maps.length || ny < 0 || nx >= maps[0].length || nx < 0) {
                     continue;
                 }
-                if(dist[ny][nx] > 0) continue;
-                if(maps[ny][nx] == 0) continue;
+                if(maps[ny][nx] == 0 || vis[ny][nx] != 0) continue;
                 
-                dist[ny][nx] = dist[cur[0]][cur[1]] + 1;
                 
-                if(ny == maxY-1 && nx == maxX-1) return dist[ny][nx];
-                
+                vis[ny][nx] = vis[cur[0]][cur[1]] + 1;
                 q.add(new int[]{ny, nx});
+                
+                if(ny == maps.length-1 && nx == maps[0].length-1) return vis[ny][nx];
             }
         }
         
