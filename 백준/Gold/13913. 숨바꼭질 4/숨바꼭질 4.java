85f9ad26c73abf68080c87b778803a5dc4b97edd
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-11-20 15:31:35 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 320 ms, Memory: 37960 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -4,62 +4,69 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.*;
 
-class Main {
-  static int N, K;
+class Main{
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N, K;
     N = Integer.parseInt(st.nextToken());
     K = Integer.parseInt(st.nextToken());
 
-    int[] prev = new int[100001];
     int[] dist = new int[100001];
-
-    Queue<Integer> q = new LinkedList<>();
-    q.add(N);
-    dist[N] = 1;
-    int cnt = 0;
+    int[] memo = new int[100001];
+    for(int i=0; i<100001; ++i) {
+      dist[i] = 100001;
+    }
+    Queue<int[]> q = new LinkedList<>();
+    q.add(new int[]{N, 0});
+    int ans = 0;
+    dist[N] = 0;
+    memo[N] = N;
     while(!q.isEmpty()) {
-      int cur = q.poll();
+      int[] cur = q.poll();
 
-      if(cur == K) {
-        break;
+      int n1 = cur[0] + 1;
+      int n2 = cur[0] - 1;
+      int n3 = cur[0] * 2;
+
+      if(cur[0] == K) {
+        ans = cur[1]; break;
       }
 
-      if(cur - 1 >= 0 && dist[cur-1] == 0) {
-        dist[cur-1] = dist[cur] + 1;
-        prev[cur-1] = cur;
-        q.add(cur-1);
+      if(n1 >= 0 && n1 <= 100000 && dist[n1] == 100001) {
+        dist[n1] = cur[1] + 1;
+        q.add(new int[]{n1, dist[n1]});
+        memo[n1] = cur[0];
       }
 
-      if(cur + 1 <= 100000 && dist[cur+1] == 0) {
-        dist[cur+1] = dist[cur] + 1;
-        prev[cur+1] = cur;
-        q.add(cur+1);
+      if(n2 >= 0 && n2 <= 100000 && dist[n2] == 100001) {
+        dist[n2] = cur[1] + 1;
+        q.add(new int[]{n2, dist[n2]});
+        memo[n2] = cur[0];
       }
 
-      if(cur*2 <= 100000 && dist[cur*2] == 0) {
-        dist[cur*2] = dist[cur] + 1;
-        prev[cur*2] = cur;
-        q.add(cur*2);
+      if(n3 >= 0 && n3 <= 100000 && dist[n3] == 100001) {
+        dist[n3] = cur[1] + 1;
+        q.add(new int[]{n3, dist[n3]});
+        memo[n3] = cur[0];
       }
     }
 
-    // 역 추적
+
     Deque<Integer> dq = new LinkedList<>();
-    dq.add(K);
-    int cur = K;
-    while(cur != N) {
-      dq.addLast(prev[cur]);
-      cur = prev[cur];
-    }
+    int current = K;
+    dq.addLast(K);
+      while(current != N) {
+        dq.addLast(memo[current]);
+        current = memo[current];
+      }
 
-    System.out.println(dq.size()-1);
-    while(!dq.isEmpty()) {
-      System.out.print(dq.pollLast() + " ");
-    }
+      bw.write(ans + "\n");
+      while(!dq.isEmpty()) {
+        bw.write(dq.pollLast() + " ");
+      }
+      bw.flush();
   }
-
 }
