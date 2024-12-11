9ccfb565aa92503447325c050a03076de5375aee
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-12-11 19:10:10 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+    long ans = 1;
+    for(int i=1; i<=N; ++i){
+      ans *= (long)i;
+    }
+
+    System.out.println(ans);
+  }
+}
