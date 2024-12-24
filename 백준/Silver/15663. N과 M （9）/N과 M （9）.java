236c491310f53408666a765f551f028a7b6fe50e
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-12-24 19:38:46 +0900

    [Silver II] Title: N과 M (9), Time: 444 ms, Memory: 75020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,76 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+public class Main {
+  static int N, M;
+  static List<Integer> list;
+  static Set<String> set;
+  static boolean[] vis;
+
+  static int[][] matrix;
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    vis = new boolean[N];
+    list = new ArrayList<>();
+    for(int i=0; i<N; ++i) {
+      list.add(Integer.parseInt(st.nextToken()));
+    }
+
+    Collections.sort(list);
+    set = new HashSet<>();
+
+    dfs(new LinkedList<>());
+
+
+    bw.flush();
+
+  }
+
+  private static void dfs(LinkedList<Integer> dq) throws Exception {
+    if(dq.size() == M) {
+      StringBuilder sb = new StringBuilder();
+      for (Integer val : dq) {
+        sb.append(val + " ");
+      }
+
+      if(set.contains(sb.toString())) {
+        return;
+      } else {
+        bw.write(sb.toString() + "\n");
+        set.add(sb.toString());
+        return;
+      }
+
+    }
+
+    for(int i=0; i<N; ++i) {
+      if(vis[i]) continue;
+
+      vis[i] = true;
+      dq.addLast(list.get(i));
+      dfs(dq);
+      dq.pollLast();
+      vis[i] = false;
+    }
+
+
+  }
+
+
+}
