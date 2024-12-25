693ef8396a2d3ff775f02fe48938968c801f3230
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2024-12-25 22:50:37 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    System.out.println(N + M);
+
+  }
+}
