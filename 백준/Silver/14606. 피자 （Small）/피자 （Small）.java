d2c76aee274de2a50776e71a33331d06dd13cb01
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2025-02-26 23:44:50 +0900

    [Silver V] Title: 피자 (Small), Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/14606. 피자 （Small）/피자 （Small）.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+
+    int ans =0;
+    for(int i=0; i<N; ++i) {
+      ans += i;
+    }
+
+    System.out.println(ans);
+  }
+
+}
