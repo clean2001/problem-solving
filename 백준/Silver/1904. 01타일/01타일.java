221a07bbedba76747a611369e039189bdac7b588
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2025-05-23 00:10:55 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,41 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
- 
-		System.out.println(Tile(N));
- 
-	}
- 
-	public static int Tile(int a) {
- 
-		if (a == 1) {
-			return 1;
-		} 
- 
-		if (a == 2) {
-			return 2;
-		}
- 
-		// 초기 값 
-		int val1 = 1;
-		int val2 = 2;
-		int sum = 0;
- 
-		for (int i = 2; i < a; i++) {
-			sum = (val2 + val1) % 15746;	// 이전 값과 이전의 이전 값의 합 
-			val1 = val2;	// 이전의 이전 값은 이전 값으로 변경 
-			val2 = sum;		// 이전 값은 현재 합 값으로 변경 
-		}
- 
-		return sum;
- 
-	}
- 
+import java.util.*;
+
+class Main {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    // 1과 00은 한쪽 방향에만 붙일 수 있다.
+    int num1 = 1;
+    int num2 = 2;
+
+
+    for(int i=3; i<=n; i++){
+      int newNum = (num1 + num2) % 15746;
+      num1 = num2;
+      num2 = newNum;
+    }
+
+    if(n == 1) System.out.println(num1);
+    else System.out.println(num2);
+  }
 }
