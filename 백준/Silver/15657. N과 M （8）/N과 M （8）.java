12b14c931da1ae9bd1d35b52ec68e84aedc5a424
Sejeong Kim <64718002+clean2001@users.noreply.github.com>
2025-06-08 12:52:30 +0900

    [Silver III] Title: N과 M (8), Time: 192 ms, Memory: 19252 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.security.MessageDigest;
+import java.util.*;
+
+class Main {
+  private static int N, M, ans = 0;
+  private static int[] arr;
+  private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    arr = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for(int i=0; i<N; ++i) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    // 정렬
+    Arrays.sort(arr);
+
+    dfs(0, new ArrayList<>());
+
+    bw.flush();
+  }
+
+  private static void dfs(int idx, List<Integer> list) throws Exception {
+      if(list.size() == M) {
+        for(int e : list) {
+          bw.write(e + " ");
+        }
+        bw.write("\n");
+        return;
+      }
+
+    for(int i=idx; i<N; ++i) {
+      list.add(arr[i]);
+      dfs(i, list);
+      list.remove(list.size()-1);
+    }
+  }
+}
